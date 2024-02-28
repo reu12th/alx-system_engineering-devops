a698ca4d71160d88a024ddfe2fc0b55724fe6606
Reuben Abdul <reu12th@gmail.com>
2024-02-28 05:10:52 +0100

    6. Call me maybe

---
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+# This task is brought to you by a professional advisor Neha Jain,
+# Senior Software Engineer at LinkedIn.
+# Requirement:
+# The regular expression must match a 10 digit phone number
+puts ARGV[0].scan(/^\d{10,10}$/).join
