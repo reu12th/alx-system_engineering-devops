342954355fa84939a2fc6215ca37597af4a69b85
Reuben Abdul <reu12th@gmail.com>
2024-02-28 05:10:38 +0100

    5. Not quite HBTN yet

---
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby
+# Requirements:
+# The regular expression must be exactly matching a string 
+# that starts with h ends with n and can have any single character in between
+# Using the project instructions, create a Ruby script that 
+# accepts one argument and pass it to a regular expression matching method
+# Example:
+puts ARGV[0].scan(/^h.n$/).join
