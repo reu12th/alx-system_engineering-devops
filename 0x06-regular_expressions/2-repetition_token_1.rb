0463a9af2ddd6ab7a6f47f399bd2fd34939e4a3f
Reuben Abdul <reu12th@gmail.com>
2024-02-28 05:29:11 +0100

    2. Repetition Token #1

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,6 +1,7 @@
-#!/usr/bin/env rubyRequirements:
+#!/usr/bin/env ruby
+# Requirements:
 # Find the regular expression that will match the above cases
 # Using the project instructions, create a Ruby script that 
 # accepts one argument and pass it to a regular expression matching method
 
-puts ARGV[0].scan(/htn|hbtn/).join
+puts ARGV[0].scan(/hb?t?n/).join
