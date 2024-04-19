af9aff8496162d1937d057b8f0c6ea1525205657
Reuben Abdul <reu12th@gmail.com>
2024-04-20 00:26:58 +0100

    2. Execute a command

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 #This script kills a process and works together with the killmenow file which has already been provided
 exec { 'killmenow':
-  command  =>  '/usr/bin/pkill killmenow'
+  command  =>  '/usr/bin/pkill killmenow',
   provider =>  'shell',
   return   =>  [0, 1],
 }
