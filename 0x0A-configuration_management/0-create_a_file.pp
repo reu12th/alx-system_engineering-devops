e2c62ca8163587899929190515764532704355cd
Reuben Abdul <reu12th@gmail.com>
2024-04-20 00:23:09 +0100

    0. Create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 #This script will create a file name 'school' inside the /tmp dir
 file { '/tmp/school':
-  ensure    =>  file,
-  content   =>  'I love Puppet',
-  mode      =>  '07744',
-  owner     =>  'www-data',
-  group     =>  'www-data',
+  ensure  =>  file,
+  content =>  'I love Puppet',
+  mode    =>  '0744',
+  owner   =>  'www-data',
+  group   =>  'www-data',
 }
