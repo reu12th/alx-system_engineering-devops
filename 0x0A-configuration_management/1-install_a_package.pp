3199fbd6487fb61b42b991d578f64443efdd402b
Reuben Abdul <reu12th@gmail.com>
2024-04-21 02:53:25 +0100

    1. Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 #install a flask from pip3
 exec { 'install flask':
-  command =>  'pip3 install flask==2.1.0',
-  path    =>  '/usr/bin/',
-  unless  =>  'pip3 list | grep flask',
+  command => 'pip3 install flask==2.1.0 werkzeug==2.1.1',
+  path    => '/usr/bin/',
+  unless  => 'pip3 list | grep flask',
 }
